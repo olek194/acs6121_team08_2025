504c80a0cd6120606a6124963f90c229b482aab9
Your Name <student@Byteasaurus.>
2025-05-13 15:09:26 +0100

    launch_file_working

--- acs6121_team08_2025/launch/explore.launch.py
+++ acs6121_team08_2025/launch/explore.launch.py
@@ -1,10 +1,14 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
 import os
-from launch.actions import IncludeLaunchDescription, TimerAction
+from launch.actions import IncludeLaunchDescription, TimerAction, ExecuteProcess
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from ament_index_python.packages import get_package_share_directory
 from launch.actions import SetEnvironmentVariable
+from launch_ros.substitutions import FindPackageShare
+from launch.substitutions import PathJoinSubstitution
+
+
 
 def generate_launch_description():
     return LaunchDescription([
@@ -34,30 +38,34 @@ def generate_launch_description():
         # ),
 
         # SLAM TOOLBOX (instead of cartographer)
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(
-                os.path.join(
-                    get_package_share_directory("slam_toolbox"),
-                    "launch", "online_async_launch.py"
-                )
-            )
-        ),
+        # IncludeLaunchDescription(
+        #     PythonLaunchDescriptionSource(
+        #         os.path.join(
+        #             get_package_share_directory("slam_toolbox"),
+        #             "launch", "online_async_launch.py"
+        #         )
+        #     )
+        # ),
 
        
      ## START NAVIGATION (Nav2)
     
             
-        #  Set the model BEFORE launching anything
-        SetEnvironmentVariable('TURTLEBOT3_MODEL', 'waffle'),
+        # #  Set the model BEFORE launching anything
+        # SetEnvironmentVariable('TURTLEBOT3_MODEL', 'waffle'),
         
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource(
-                os.path.join(
-                    get_package_share_directory("turtlebot3_navigation2"),
-                    "launch", "navigation2.launch.py"
-                )
+                PathJoinSubstitution([
+                    FindPackageShare("turtlebot3_navigation2"),
+                    "launch",
+                    "navigation2.launch.py"
+                ])
             ),
-            launch_arguments={'slam': 'True'}.items()
+            launch_arguments={
+                'slam': 'True',
+                'params_file': '/home/student/ros2_ws/src/acs6121_team08_2025/scripts/waffle.yaml'
+            }.items()
         ),
             
         
@@ -93,7 +101,22 @@ def generate_launch_description():
         #     ]
         # ),
 
-        # # MAP SAVER CLIENT
+        
+
+        ## WAYPOINT NAVIGATOR (Sending multiple waypoints)
+        TimerAction(
+             period=10.0,
+             actions=[
+                Node(
+                    package='acs6121_team08_2025',  # Replace with your package
+                    executable='waypoint_navigator.py',  # Script that sends waypoints
+                    name='waypoint_navigator',
+                    output='screen'
+                ),
+             ]
+        ),
+            
+            # MAP SAVER CLIENT
         # TimerAction(
         #     period=25.0,
         #     actions=[
@@ -108,20 +131,17 @@ def generate_launch_description():
         #         )
         #     ]
         # ),
-
-        ## WAYPOINT NAVIGATOR (Sending multiple waypoints)
+   
         TimerAction(
-             period=5.0,
-             actions=[
-                Node(
-                    package='acs6121_team08_2025',  # Replace with your package
-                    executable='waypoint_navigator.py',  # Script that sends waypoints
-                    name='waypoint_navigator',
+            period=95.0,
+            actions=[
+                ExecuteProcess(
+                    cmd=['ros2', 'run', 'nav2_map_server', 'map_saver_cli', '-f', '/home/student/ros2_ws/src/acs6121_team08_2025/maps/explore_map'],
                     output='screen'
-                ),
-             ]
-        ),
-            
+                )
+            ]
+        )
+
         
     
     ])
